474051c061c431100e161012c565a28158113a09
Anh Hoang <anhh3@fsoft.com.vn>
2017-01-11 11:00:51 +0700

    add app id

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ server.listen(process.env.PORT || 3000, function()
 });
 
 // Create chat bot
-var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID = '45b599be-3105-4b1f-9051-6b6f63cae663',
+    appPassword: process.env.MICROSOFT_APP_PASSWORD = 'hBEkL4HpcMxG4PpXdAgxpKq'
+});
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
